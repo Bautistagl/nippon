94a1ed9cf689593d45e1ebf17e9f8a27223002ab
Bautistagl <bautistagonzalezlazo@gmail.com>
2023-09-27 17:24:55 -0300

    dsad

--- nippon/src/pages/api/notificaciones.js
+++ nippon/src/pages/api/notificaciones.js
@@ -3,7 +3,7 @@
 const notificaciones = async (req,res) => {
     if (req.method === "POST") {
         console.log(req.body)
-        res.status(200).sned('OK')
+        res.status(200).send('OK')
     }
     else {
         res.status(400).json({ message: "Method not allowed" })
