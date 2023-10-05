8758a1277811c9b93661dfabd5d6162ce9160441
Bautistagl <bautistagonzalezlazo@gmail.com>
2023-10-05 16:09:04 -0300

    dasdsa

--- nippon/src/pages/api/webhook.js
+++ nippon/src/pages/api/webhook.js
@@ -2,17 +2,28 @@ import back from "@/config2/axiosbautista";
 
 export default async function handler(req, res) {
     if (req.method === 'POST') {
-      // Verifica la presencia del campo data
-      const data = req.body;
-      if (!data) {
-        res.status(400).json({ message: 'El campo data es obligatorio' });
-        return;
+      
+      const handleMail = async () => {
+        try {
+          const paymentData = req.body;
+          // const email = paymentData.payer.email;
+          const mailUsuario= {
+            // Aquí puedes definir los datos que deseas enviar al backend
+            mail: paymentData,
+            
+           
+            // ...
+          };
+          const response2 = await back.post('/nodemailerSend',mailUsuario);
+          // const response = await back.post('/avisoPedido',mailUsuario);
+      
+        } catch (error) {
+          console.error(error);  // Maneja cualquier error que ocurra durante la solicitud
+        }
       }
-
-      // ...
-
+      await handleMail();
       res.status(200).send('OK');
     } else {
       res.status(400).json({ message: 'Method not allowed' });
     }
-}
+}  
