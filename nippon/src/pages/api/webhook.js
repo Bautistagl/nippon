10693dd1f527a906b5bd5b468b0e4774e6ad0309
Bautistagl <bautistagonzalezlazo@gmail.com>
2023-10-05 17:27:33 -0300

    dsadas

--- nippon/src/pages/api/webhook.js
+++ nippon/src/pages/api/webhook.js
@@ -1,4 +1,5 @@
 import back from "@/config2/axiosbautista";
+const nodemailer = require("nodemailer");
 
 export default async function handler(req, res) {
   const { method, body } = req;
@@ -27,5 +28,51 @@ export default async function handler(req, res) {
       }
       await handleMail();
       res.status(200).send('OK');
+    }
+    case "GET": {
+      
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: "bautistagonzalezlazo@gmail.com",
+          pass: "ybnc fgfs pcqi rxra",
+        },
+      });
+
+      const mailOptions = {
+        from: "Nippon",
+        to: `bautistagonzalezlazo@gmail.com`,
+        subject: "Formulario distribuidor",
+        text:`Estimado get,
+         Muchas gracias por su interés en nuestros productos.
+         Nosotros tenemos 2 grandes líneas de productos: 
+         -Sakura: son macetas desmoldables
+         -Origami: son macetas desmoldables
+         Te voy a pedir que me pases los siguientes datos:
+         -Estructura
+         -Que línea de macetas les interesa.
+         -Hace cuánto están en el rubro?
+         -¿Cuentan con página Web?
+         -¿Son responsables insscriptos?
+          Te solicito la razón social
+          Saludos cordsales,
+          Nippon`,
+          
+       
+      };
+
+      try {
+        await transporter.sendMail(mailOptions);
+        console.log("Coraaaareo enviado");
+        res.status(200).send({
+          email: null,
+          nick_name: null,
+          id: "1",
+          message: "Correo enviado exitosamente",
+        });
+      } catch (error) {
+        console.error("Error:", error);
+        res.status(500).send("Error al enviar el correo");
+      }
     }
     } }
