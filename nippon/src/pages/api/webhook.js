bc6da77430b6a9be0b80d8924b1622ac120276e5
Bautistagl <bautistagonzalezlazo@gmail.com>
2023-10-05 16:02:30 -0300

    dsadsa

--- nippon/src/pages/api/webhook.js
+++ nippon/src/pages/api/webhook.js
@@ -2,35 +2,17 @@ import back from "@/config2/axiosbautista";
 
 export default async function handler(req, res) {
     if (req.method === 'POST') {
-      const data = req.body
-      // const paymentData = req.body;
-      // const data = {
-      //   id: paymentData.id,
-      //   amount: paymentData.amount,
-      //   currency: paymentData.currency,
-      //   // ...
-      // };
-      // const handleMail = async () => {
-      //   try {
-      //     const paymentData = req.body;
-      //     // const email = paymentData.payer.email;
-      //     const mailUsuario= {
-      //       // Aquí puedes definir los datos que deseas enviar al backend
-      //       mail: paymentData,
-            
-           
-      //       // ...
-      //     };
-      //     const response = await back.post('/avisoPedido',paymentData);
-      //     const response2 = await back.post('/nodemailerSend',mailUsuario);
-      
-      //   } catch (error) {
-      //     console.error(error);  // Maneja cualquier error que ocurra durante la solicitud
-      //   }
-      // }
-      // await handleMail();
+      // Verifica la presencia del campo data
+      const data = req.body;
+      if (!data) {
+        res.status(400).json({ message: 'El campo data es obligatorio' });
+        return;
+      }
+
+      // ...
+
       res.status(200).send('OK');
     } else {
       res.status(400).json({ message: 'Method not allowed' });
     }
-}  
+}
