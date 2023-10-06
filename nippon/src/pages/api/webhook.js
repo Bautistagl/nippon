455720ee28abe6471341cc0bec6a7a803250e2b5
Bautistagl <bautistagonzalezlazo@gmail.com>
2023-10-06 16:57:12 -0300

    dsdas

--- nippon/src/pages/api/webhook.js
+++ nippon/src/pages/api/webhook.js
@@ -1,45 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
 
-const WebhookPage = () => {
-  const [webhookData, setWebhookData] = useState(null);
-  const router = useRouter();
 
-  useEffect(() => {
-    // Escucha las solicitudes HTTP POST en la ruta /webhook
-    const handleWebhook = async (req, res) => {
-      // Procesa el webhook
-      setWebhookData(req.body);
+export default async function handler(req, res) {
+  const { method, body } = req;
+  
+  switch (method) {
+    case "POST": {
 
-      // Responde con un código de estado 201 (Creado)
-      res.status(201).send('OK');
-    };
-
-    router.events.on('fetch', handleWebhook);
-
-    return () => {
-      // Elimina el manejador de eventos
-      router.events.off('fetch', handleWebhook);
-    };
-  }, []);
-
-  if (!webhookData) {
-    return <div>Esperando webhook...</div>;
-  }
-
-  // Procesa el webhook
-
-  // ...
-
-  return <div>
-    <h1>Webhook recibido</h1>
-    <p>
-      El estado del pago es: {webhookData.estado}
-    </p>
-    <p>
-      El monto del pago es: {webhookData.monto}
-    </p>
-  </div>;
-};
-
-export default WebhookPage;
+      const data = body
+      console.log(data)
+      
+      res.status(200).send('OK');
+    }
+   
+    } }
